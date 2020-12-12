f8c9307c573e09b8e6a0a91afc3cfd903426cdbc
Berari Mihai-Dan <berari.mihaidan@gmail.com>
2020-12-12 23:04:38 +0200

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FilmComponent } from './pages/film/film.component';
+import { ConclusionComponent } from './pages/conclusion/conclusion.component';
+import { Film2Component } from './pages/film2/film2.component';
+import { Film3Component } from './pages/film3/film3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilmComponent,
+    ConclusionComponent,
+    Film2Component,
+    Film3Component
   ],
   imports: [
     BrowserModule,
