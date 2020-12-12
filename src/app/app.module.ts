94193d9bc1974a67ccc4eeae8c3d42a15f989f98
Berari Mihai-Dan <berari.mihaidan@gmail.com>
2020-12-13 00:40:39 +0200

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
-import { FilmComponent } from './pages/film/film.component';
 import { ConclusionComponent } from './pages/conclusion/conclusion.component';
 import { Film2Component } from './pages/film2/film2.component';
 import { Film3Component } from './pages/film3/film3.component';
+import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
+import { Film1Component } from './pages/film1/film1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FilmComponent,
+    Film1Component,
     ConclusionComponent,
     Film2Component,
-    Film3Component
+    Film3Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
+    NavbarModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/film1/film1.component.spec.ts
+++ src/app/pages/film1/film1.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FilmComponent } from './film.component';
+import { Film1Component } from './film1.component';
 
-describe('FilmComponent', () => {
-  let component: FilmComponent;
-  let fixture: ComponentFixture<FilmComponent>;
+describe('Film1Component', () => {
+  let component: Film1Component;
+  let fixture: ComponentFixture<Film1Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FilmComponent ]
+      declarations: [ Film1Component ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FilmComponent);
+    fixture = TestBed.createComponent(Film1Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
